4a8fa36127a24ffcd4d9c8592165921367cd39d1
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-10-22 20:33:50 +0100

    Generating Entities from Database Schema

---
+++ src/main/java/entity/Employee.java
@@ -0,0 +1,77 @@
+package entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Employee {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id")
+    private int id;
+    @Basic
+    @Column(name = "firstName")
+    private String firstName;
+    @Basic
+    @Column(name = "lastName")
+    private String lastName;
+    @Basic
+    @Column(name = "department_id")
+    private Integer departmentId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Integer departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Employee employee = (Employee) o;
+
+        if (id != employee.id) return false;
+        if (firstName != null ? !firstName.equals(employee.firstName) : employee.firstName != null) return false;
+        if (lastName != null ? !lastName.equals(employee.lastName) : employee.lastName != null) return false;
+        if (departmentId != null ? !departmentId.equals(employee.departmentId) : employee.departmentId != null)
+            return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + (departmentId != null ? departmentId.hashCode() : 0);
+        return result;
+    }
+}
